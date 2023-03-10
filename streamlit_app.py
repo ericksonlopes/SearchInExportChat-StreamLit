a3da26968a339e4e167b4957332caeb40d9cd8b2
Erickson Lopes <ofc.erickson@gmail.com>
2023-03-10 11:21:31 -0300

    init project

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,18 @@ import streamlit as st
 
 from clear_file import BaseClearDataFile
 
-data = st.file_uploader('Escolha o arquivo', type=['txt'])
+data = st.file_uploader('Escolha o arquivo para analise de dados', type=['txt'])
 
 if data is None:
     st.stop()
 
+# read file as a string data
 file = data.getvalue().decode("utf-8").split("\n")
+
+# clear data
 base = BaseClearDataFile(file_data=file)
 
+# show dataframes
 st.title('Messages')
 df = pd.DataFrame([m.__dict__ for m in base.messages])
 st.write(df)
