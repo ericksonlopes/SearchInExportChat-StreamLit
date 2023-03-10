194e7329d896e575a1330de5034ab33b56d282b4
Erickson Lopes <ofc.erickson@gmail.com>
2023-03-10 10:51:21 -0300

    init project

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from clear_file import BaseClearDataFile
 data = st.file_uploader('Escolha o arquivo', type=['txt'])
 
 if data is not None:
-    base = BaseClearDataFile(file_data=data)
+    # base = BaseClearDataFile(file_data=data)
 
     st.write(data.getvalue())
 
