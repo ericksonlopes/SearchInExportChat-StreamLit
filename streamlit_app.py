9f329ad9e0d108469921aec68b8bbec7f8adea67
Erickson Lopes <ofc.erickson@gmail.com>
2023-03-10 11:12:40 -0300

    init project

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,16 @@ from clear_file import BaseClearDataFile
 
 data = st.file_uploader('Escolha o arquivo', type=['txt'])
 
-if data is not None:
-    file = data.getvalue().decode("utf-8").split("\n")
-    base = BaseClearDataFile(file_data=file)
+if data is None:
+    st.stop()
 
-    st.title('Messages')
-    df = pd.DataFrame([m.__dict__ for m in base.messages])
-    st.write(df)
+file = data.getvalue().decode("utf-8").split("\n")
+base = BaseClearDataFile(file_data=file)
 
-    st.title('Info Messages')
-    df = pd.DataFrame([m.__dict__ for m in base.info_messages])
-    st.write(df)
+st.title('Messages')
+df = pd.DataFrame([m.__dict__ for m in base.messages])
+st.write(df)
+
+st.title('Info Messages')
+df = pd.DataFrame([m.__dict__ for m in base.info_messages])
+st.write(df)
